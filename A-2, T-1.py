5bea43411101dc9507659831777e2cb1e034d773
lolodidler <anooppahariya777@gmail.com>
2025-11-22 22:34:08 +0530

    Add files via upload

---
+++ A-2, T-1.py
@@ -0,0 +1,10 @@
+#task is to find if a number is even or odd.
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print(f"The number you entered({number}) is even.")
+else:
+    print(f"The number you entered({number}) is odd.")
+
+#task is done.
---
+++ A-2, T-2.py
@@ -0,0 +1,4 @@
+# task is to create code that gives the sum of all numbers from 1 to 50
+
+total = sum(range(1, 51))
+print(f"the sum of all the numbers from 1 to 50 is: {total}")
